b406f2e9fcddf494395918b2ba51528f206c2923
ram-a-dhan <ram.adhan@outlook.com>
2020-07-25 17:14:23 +0700

    refined notification; added unanimated hide function to notification;

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Notification.css';
 
 export default function Notification() {
+  const [hideNotif, setHideNotif] = useState(false);
+
   return (
-    <div className="Notification">
+    <div className={"Notification" + (hideNotif ? " NotifHidden" : "")}>
       <div className="NotifBox">
         <p>
-          By accessing and using this website, 
-          you acknowledge that you have read and 
-          understand our Cookie Policy, Privacy Policy, 
-          and our Terms of Service.
+          By accessing and using this website, you acknowledge that you have read and understand our
+          {/* eslint-disable-next-line */}
+          &nbsp;<a href="">Cookie Policy</a>, <a href="">Privacy Policy</a>, and our <a href="">Terms of Service</a>.
         </p>
-        <button>
+        <button onClick={() => setHideNotif(true)}>
           Got it
         </button>
       </div>
