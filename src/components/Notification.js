ef33a4dd6ccf71e62fe298645a9ccaa39db3e919
ram-a-dhan <ram.adhan@outlook.com>
2020-07-28 10:08:36 +0700

    added close animation to notification;

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './Notification.css';
 
 export default function Notification({ hideNotif, setHideNotif, ...props }) {
+  const handleHideNotif = () => {
+    document.querySelector('.Notification').classList.toggle('NotifSlideUp');
+  };
   return (
-    <div className={"Notification" + (hideNotif ? " NotifHidden" : "")}>
+    // <div className={"Notification" + (hideNotif ? " NotifHidden" : "")}>
+    <div className="Notification">
       <div className="NotifBox">
         <p>
           By accessing and using this website, you acknowledge that you have read and understand our
           {/* eslint-disable-next-line */}
           &nbsp;<a href="">Cookie Policy</a>, <a href="">Privacy Policy</a>, and our <a href="">Terms of Service</a>.
         </p>
-        <button onClick={() => setHideNotif(true)}>
+        {/* <button onClick={() => setHideNotif(true)}> */}
+        <button onClick={() => handleHideNotif()}>
           Got it
         </button>
       </div>
