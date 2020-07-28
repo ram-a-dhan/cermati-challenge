e880f8f53eeac9d04bebf7657188dadcf9f74ee2
ram-a-dhan <ram.adhan@outlook.com>
2020-07-28 19:24:20 +0700

    obtained scroll position indicator;

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Newsletter.css';
 
 export default function Newsletter({ hideNews, setHideNews, ...props }) {
+  const [scrollPos, setScrollPos] = useState(0);
+  const [docHeight, setDocHeight] = useState(0);
+
+  useEffect(() => {
+    const handleDocHeight = (event) => {
+      setDocHeight(getDocHeight);
+    };
+    window.addEventListener('load', handleDocHeight);
+
+    const handleScrollPos = (event) => {
+      // setScrollPos(event.target.documentElement.scrollTop);
+      setScrollPos(window.pageYOffset);
+    };
+    window.addEventListener('scroll', handleScrollPos);
+    return () => {
+      window.removeEventListener('load', handleDocHeight);
+      window.removeEventListener('scroll', handleScrollPos);
+    }
+  }, [scrollPos, docHeight]);
+
+  const getDocHeight = () => {
+    setDocHeight(Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.body.clientHeight, document.documentElement.clientHeight
+    ))
+  }
+
   const handleHideNews = () => {
     document.querySelector('.Newsletter').classList.toggle('NewsSlideDown');
   };
+  
   return (
     <div className="NewsContainer">
       {/* <div className={"Newsletter" + (hideNews ? " NewsHidden" : "")}> */}
@@ -14,7 +43,7 @@ export default function Newsletter({ hideNews, setHideNews, ...props }) {
           &times;
         </button>
         <h2>
-          Get latest updates in web technologies
+          Get latest updates in web technologies {scrollPos}/{docHeight}
         </h2>
         <p>
           I write articles related to web technologies, such as design trends, 
